44102fafbc4b55ef9078cebe7e3b846451195cf0
luiizsilverio <luiiz.silverio@gmail.com>
2022-12-21 20:46:03 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, Notification } = require("electron");
+const path = require("path");
+
+let mainWindow = null;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+    //   webPreferences: {
+    //   preload: path.join(__dirname, "preloader.js")
+    // }
+	});
+  mainWindow.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on("active", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	})
+
+  enviaNotificacao();
+})
+
+app.on("window-all-closed", () => {
+	if (process.plataform !== "darwin") app.quit();
+})
+
+function enviaNotificacao() {
+  if (!Notification.isSupported()) return;
+
+  const notification = new Notification({
+    title: 'Título da Notificação',
+    subtitle: 'Exemplo de uso de notificações no Electron', // só para Mac
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exemplo de notificação',
+    silent: false,
+    icon: path.join(__dirname, 'public', 'logo192.png'),
+    timeoutType: "default" // default ou never
+  })
+
+  notification.on("show", () => console.log('Notification show'));
+  notification.on("click", () => console.log('Notification click'));
+  notification.on("close", () => console.log('Notification close'));
+  notification.on("failed", () => console.log('Notification failed'));
+  notification.show();
+}
--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exemplo de uso do Electron com React.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/luiizsilverio"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LuizDev
         </a>
       </header>
     </div>
